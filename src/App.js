c82ee27f25ffae6e18b05f7bf769d47bbc50885e
GigiriOne <derda.derda@mail.ru>
2023-03-20 21:53:25 +0500

    Добавил новые файлы (add new file)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import AaaComponents from './components/AaaComponents';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <AaaComponents />
+      <MyComponent />
+      <AaaComponents />
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AaaComponents.js
@@ -0,0 +1,7 @@
+function AaaComponents() {
+    return(
+        <p>Privet vsem, Pishy vsem, Kak vsem, Zachem vsem</p>
+    )
+}
+
+export default AaaComponents
